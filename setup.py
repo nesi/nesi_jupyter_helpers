64b498aed2baa9f860e586d9c694af01ecf59314
Maxime RIO <maxime.rio@niwa.co.nz>
2021-07-19 22:13:59 +0000

    add entry-point script

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nesi_jupyter_helpers',
-    version='0.1.0',
+    name="nesi_jupyter_helpers",
+    version="0.1.0",
     description="Tools to help users to use NeSI's JupyterHub",
-    url='https://github.com/nesi/nesi_jupyter_helpers',
-    author=' New Zealand eScience Infrastructure',
-    author_email='support@nesi.org.nz',
-    license='MIT',
+    url="https://github.com/nesi/nesi_jupyter_helpers",
+    author=" New Zealand eScience Infrastructure",
+    author_email="support@nesi.org.nz",
+    license="MIT",
     packages=find_packages(),
-    install_requires=['ipykernel'],
-    extra_require={
-        "dev": ["black", "flake8"]
-    }
+    install_requires=["ipykernel"],
+    extras_require={"dev": ["black", "flake8"]},
+    entry_points={
+        "console_scripts": ["nesi-add-kernel=nesi_jupyter_helpers.add_kernel:main"]
+    },
 )
