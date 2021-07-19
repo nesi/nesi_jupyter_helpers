6d46a1376e7f0287e06368b31cef106b77d1f67e
Maxime RIO <maxime.rio@niwa.co.nz>
2021-07-19 10:13:58 +0000

    add an option to register a conda environment

--- jupyter_kernel_add.py
+++ jupyter_kernel_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import json
 import subprocess
 import typing as T
@@ -24,34 +25,53 @@ CONDA_TEMPLATE = """\
 
 # load conda & CUDA modules on Mahuika or Maui
 if hostname | grep -q "maui"; then
-    CONDA_MODULE="Anaconda3"
+    module load Anaconda3
 else
-    CONDA_MODULE="Miniconda3"
+    module load Miniconda3
 fi
-module load "$CONDA_MODULE"
 
 # activate conda environment
 source $(conda info --base)/etc/profile.d/conda.sh
 conda deactivate  # enforce base environment to be unloaded
-conda activate {CONDA_VENV_PATH}
+conda activate {conda_venv}
 
 """
 
 
-def parse_args(args: T.Optional[T.Iterable[str]] = None) -> Namespace:
+def parse_args() -> Namespace:
     """Command line input parser"""
+
     parser = ArgumentParser(
-        description="register a new Jupyter kernel",
+        description="register a new Jupyter kernel, with a wrapper script to "
+        "load NeSI modules",
     )
     parser.add_argument("kernel_name", help="Jupyter kernel name")
     parser.add_argument(
         "module", nargs="+", help="NeSI module to load before running the kernel"
     )
-    return parser.parse_args(args)
+    parser.add_argument("--conda-path", help="path to a Conda environment")
+    parser.add_argument("--conda-name", type=Path, help="name of a Conda environment")
+
+    args = parser.parse_args()
+
+    if args.conda_path is not None and args.conda_name is not None:
+        sys.exit("error: --conda-path and --conda-name options are not compatible")
+    elif args.conda_path is not None:
+        args.conda_venv = str(Path(args.conda_path).resolve())
+    elif args.conda_name is not None:
+        args.conda_venv = args.conda_name
+    else:
+        args.conda_venv = None
+
+    return args
 
 
-def main(kernel_name: str, modules: T.Iterable[str]):
-    """Register a new jupyter kernel, with a wrapper script to load modules"""
+def main(
+    kernel_name: str,
+    modules: T.Iterable[str],
+    conda_venv: T.Optional[str] = None,
+):
+    """Register a new jupyter kernel, with a wrapper script to load NeSI modules"""
 
     # create a new kernel
     subprocess.run(
@@ -62,7 +82,14 @@ def main(kernel_name: str, modules: T.Iterable[str]):
     kernel_dir = Path.home() / ".local/share/jupyter/kernels/" / kernel_name
 
     # add a bash wrapper script
-    wrapper_script_code = WRAPPER_TEMPLATE.format(conda="", modules=" ".join(modules))
+    if conda_venv is None:
+        conda = ""
+    else:
+        conda = CONDA_TEMPLATE.format(conda_venv=conda_venv)
+
+    wrapper_script_code = WRAPPER_TEMPLATE.format(
+        conda=conda, modules=" ".join(modules)
+    )
     wrapper_script = kernel_dir / "wrapper.bash"
     wrapper_script.write_text(wrapper_script_code)
     wrapper_script.chmod(0o770)
@@ -85,4 +112,4 @@ def main(kernel_name: str, modules: T.Iterable[str]):
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.kernel_name, args.module)
+    main(args.kernel_name, args.module, args.conda_venv)
