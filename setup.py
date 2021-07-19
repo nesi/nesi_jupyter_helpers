09515cca5c9b7cd7f7ace3d356fa98e7d245e7a9
Maxime Rio <maxime.rio@niwa.co.nz>
2021-07-20 10:40:04 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="nesi_jupyter_helpers",
     version="0.1.0",
-    description="Tools to help users to use NeSI's JupyterHub",
+    description="Tools to help users of jupyter.nesi.org.nz",
     url="https://github.com/nesi/nesi_jupyter_helpers",
     author=" New Zealand eScience Infrastructure",
     author_email="support@nesi.org.nz",
