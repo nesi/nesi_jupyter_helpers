6427e03671d28369f6da7ede77fc221fc606f202
Maxime RIO <maxime.rio@niwa.co.nz>
2021-07-19 09:41:26 +0000

    turn the script into a CLI tool

--- jupyter_kernel_add.py
+++ jupyter_kernel_add.py
@@ -2,8 +2,9 @@
 
 import json
 import subprocess
+import typing as T
 from pathlib import Path
-
+from argparse import ArgumentParser, Namespace
 
 WRAPPER_TEMPLATE="""\
 #!/usr/bin/env bash
@@ -36,36 +37,54 @@ conda activate {CONDA_VENV_PATH}
 
 """
 
-kernel_name = "test_script"
-modules = ["TensorFlow/2.4.1-gimkl-2020a-Python-3.8.2"]
 
-# create a new kernel
-subprocess.run(
-    ["python", "-m", "ipykernel", "install", "--user", "--name", kernel_name],
-    check=True
-)
+def parse_args(args: T.Optional[T.Iterable[str]] = None) -> Namespace:
+    """Command line input parser"""
+    parser = ArgumentParser(
+        description="register a new Jupyter kernel",
+    )
+    parser.add_argument("kernel_name", help="Jupyter kernel name")
+    parser.add_argument(
+        "module", nargs="+", help="NeSI module to load before running the kernel"
+    )
+    return parser.parse_args(args)
 
-kernel_dir = Path.home() / ".local/share/jupyter/kernels/" / kernel_name
 
-# add a bash wrapper script
-wrapper_script_code = WRAPPER_TEMPLATE.format(
-    conda="", modules=" ".join(modules)
-)
-wrapper_script = kernel_dir / "wrapper.bash"
-wrapper_script.write_text(wrapper_script_code)
-wrapper_script.chmod(0o770)
+def main(kernel_name: str, modules: T.Iterable[str]):
+    """Register a new jupyter kernel, with a wrapper script to load modules"""
 
-# modify the kernel description file
-kernel_def = {
-     "argv": [
-         str(wrapper_script),
-         "-m",
-         "ipykernel_launcher",
-         "-f",
-         "{connection_file}"
-     ],
-     "display_name": kernel_name,
-     "language": "python"
-}
-with (kernel_dir / "kernel.json").open("w") as fd:
-    json.dump(kernel_def, fd, indent=4)
+    # create a new kernel
+    subprocess.run(
+        ["python", "-m", "ipykernel", "install", "--user", "--name", kernel_name],
+        check=True
+    )
+
+    kernel_dir = Path.home() / ".local/share/jupyter/kernels/" / kernel_name
+
+    # add a bash wrapper script
+    wrapper_script_code = WRAPPER_TEMPLATE.format(
+        conda="", modules=" ".join(modules)
+    )
+    wrapper_script = kernel_dir / "wrapper.bash"
+    wrapper_script.write_text(wrapper_script_code)
+    wrapper_script.chmod(0o770)
+
+    # modify the kernel description file
+    kernel_def = {
+         "argv": [
+             str(wrapper_script),
+             "-m",
+             "ipykernel_launcher",
+             "-f",
+             "{connection_file}"
+         ],
+         "display_name": kernel_name,
+         "language": "python"
+    }
+    with (kernel_dir / "kernel.json").open("w") as fd:
+        json.dump(kernel_def, fd, indent=4)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args.kernel_name, args.module)
